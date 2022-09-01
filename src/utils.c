f186fffd1637bc79a4ba9a7f13745cb6fef75808
TaleaX <sophie.dehne01@gmail.com>
2022-09-01 16:42:26 +0200

    update utils

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/06 16:25:20 by mschlenz          #+#    #+#             */
-/*   Updated: 2022/09/01 15:07:21 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/09/01 16:41:59 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,40 +33,48 @@ bool	ft_strstr2(const char *s, char *set)
 	return (false);
 }
 
-char	*ft_strstr(char *s, char *set)
+void	set_btree_value(char *s, char *set, t_btree **btree)
 {
 	int	i;
 	char	*left;
 	char	*right;
-	t_btree *node1;
-	t_btree *node2;
-	t_btree *btree = NULL;
+	t_btree *node_right;
+	t_btree *node_left;
 	int		index;
+	int		len;
 
 
 	i = 0;
-	while (s[i] != '\0')
+	len = ft_strlen(s);
+	while (len--)
 	{
-		if (ft_strncmp(s + i, set, 2) == 0)
+		if (ft_strncmp(s + len, set, 2) == 0)
 		{
-			if (!btree)
-				btree = ft_mslstnew2(set);
-			node1 = ft_mslstnew2(ft_substr(s, i + 2, ft_strlen(s - i)));
-			ft_mslstadd_right(&btree, node1);
+			if (!*btree)
+				(*btree) = ft_mslstnew2(set);
+			node_right = ft_mslstnew2(ft_substr(s, len + 2, ft_strlen(s - len)));
+			ft_mslstadd_right(btree, node_right);
 			break;
 		}
-		i++;
 	}
-	node2 = ft_mslstnew2(ft_substr(s, 0, i));
-	ft_mslstadd_left(&btree, node2);
-	printf("STR:    %s\nVAL:    %s\nLEFT:   %s\nRIGHT: %s\n",s , set, btree->left->value, node1->value);
-	return (0);
+	node_left = ft_mslstnew2(ft_substr(s, 0, len));
+	ft_mslstadd_left(btree, node_left);
+	//printf("STR:    %s\nVAL:    %s\nLEFT:   %s\nRIGHT: %s\n",s , set, (*btree)->left->value, (*btree)->right->value);
 }
 
 void	prioritization(t_data *data)
 {
 	// char 	*blub = ft_strdup("ls && grep && top && bla");
-	ft_strstr("ls | grep || bla", "||");
+	t_btree	*btree = NULL;
+
+	set_btree_value("ls | grep || bla || lol || more", "||", &btree);
+	printf("VAL:    %s\nLEFT:   %s\nRIGHT: %s\n", btree->value, btree->left->value, btree->right->value);
+	while (btree->left && ft_strstr2(btree->left->value, "||"))
+	{
+		set_btree_value(btree->left->value, "||", &btree);
+		printf("VAL:    %s\nLEFT:   %s\nRIGHT: %s\n", btree->value, btree->left->value, btree->right->value);
+		btree = btree->left;
+	}
 	// while (ft_strstr2(btree->right->value, "&&"))
 	// 	ft_strstr(btree->right->value, "&&", btree);
 }
