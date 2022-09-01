42c110b778d9ba2a9fc81640cdd666536ba9fdc9
TaleaX <sophie.dehne01@gmail.com>
2022-09-01 13:10:42 +0200

    list new

--- src/utils_lst2.c
+++ src/utils_lst2.c
@@ -6,7 +6,7 @@
 /*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/28 18:06:11 by mschlenz          #+#    #+#             */
-/*   Updated: 2022/08/31 18:04:12 by tdehne           ###   ########.fr       */
+/*   Updated: 2022/09/01 13:10:21 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,18 @@ t_env	*ft_mslstnew(t_data *data, char *var, char *content)
 	list->content = content;
 	list->next = NULL;
 	list->last = list;
+	return (list);
+}
+
+t_btree	*ft_mslstnew(char *content)
+{
+	t_btree	*list;
+
+	list = malloc(sizeof(t_btree));
+	if (!list)
+		return (NULL);
+	list->value = content;
+	list->left = NULL;
+	list->right = NULL;
 	return (list);
 }
