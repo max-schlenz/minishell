61b907895444c7e85ea010a0fb89f5c787661bb1
TaleaX <sophie.dehne01@gmail.com>
2022-09-04 13:59:43 +0200

    update p_envp

--- src/parse/p_envp.c
+++ src/parse/p_envp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   p_envp.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mschlenz <mschlenz@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: tdehne <tdehne@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 17:01:42 by mschlenz          #+#    #+#             */
-/*   Updated: 2022/09/04 13:24:10 by mschlenz         ###   ########.fr       */
+/*   Updated: 2022/09/04 13:58:59 by tdehne           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,25 @@
 void	parse_path(t_data *data)
 {
 	int		i;
-	t_env	*tmp_env;
 
 	i = 0;
-	tmp_env = data->env;
 	if (data->path)
 	{
 		while (*data->path)
 			free (*data->path++);
 	}
-	while (ft_strncmp(tmp_env->var, "PATH", 5) && ++i <= data->counter_envv)
-		tmp_env = tmp_env->next;
-	data->path = ft_split(tmp_env->content, ':');
-
+	while (data->envp[i] && ft_strncmp(data->envp[i], "PATH", 5))
+		i++;
+	data->path = ft_split(data->envp[i], ':');
 }
 
 void	parse_envp(t_data *data, char **envp)
 {
 	int		i;
 
-	data->envp = ft_calloc(data->counter_envv + 1, sizeof(char *));
-	i = 0;
-	while (i < data->counter_envv)
-	{
-		data->envp[i] = ft_strdup(envp[i]);
+	while (envp[i])
 		i++;
-	}
-	// data->envp[i] = NULL;
+	data->envp = ft_calloc(i + 1, sizeof(char *));
+	i = 0;
+	data->envp = envp;
 }
